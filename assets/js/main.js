45f0c6f0026b1540909f03263c8c1621a9cc0d2e
khaisendefo <khaisendefo@gmail.com>
2024-10-19 01:20:40 +0300

    Created header and mobile version header

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleButton = document.querySelector('.header__burger');
+  const nav = document.querySelector('.header__content');
+  const navLinks = document.querySelectorAll('.header__nav-link');
+  const overlay = document.querySelector('.header__overlay');
+  const body = document.body;
+
+  function closeNav() {
+    toggleButton.classList.remove('active');
+    nav.classList.remove('active');
+    overlay.classList.remove('active');
+    body.classList.remove('no-scroll');
+  }
+
+  toggleButton.addEventListener('click', function () {
+    const isActive = toggleButton.classList.toggle('active');
+    nav.classList.toggle('active');
+    overlay.classList.toggle('active');
+    if (isActive) {
+      body.classList.add('no-scroll');
+    } else {
+      body.classList.remove('no-scroll');
+    }
+  });
+
+  document.addEventListener('click', function(event) {
+    if (!nav.contains(event.target) && !toggleButton.contains(event.target) && !overlay.contains(event.target)) {
+      closeNav();
+    }
+  });
+
+  overlay.addEventListener('click', closeNav);
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape' && nav.classList.contains('active')) {
+      closeNav();
+    }
+  });
+
+  navLinks.forEach(function(navLink) {
+    navLink.addEventListener('click', function() {
+      closeNav();
+    });
+  });
+});
