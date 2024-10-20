04ec25accdb7ad6dccb1353dcc5814e48dd4aea1
khaisendefo <khaisendefo@gmail.com>
2024-10-20 13:47:19 +0300

    Created more section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+
+// burger
 document.addEventListener('DOMContentLoaded', function () {
   const toggleButton = document.querySelector('.header__burger');
   const nav = document.querySelector('.header__content');
@@ -43,3 +45,69 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+
+
+// reviews__slider
+const splide = new Splide('.reviews__slider', {
+   perPage: 3, 
+   perMove: 1,
+   pagination: false,
+   arrows: false,
+   gap: '13px',
+});
+
+splide.mount();
+
+const prevButton = document.querySelector('.reviews__slider-nav-arrow--prev');
+const nextButton = document.querySelector('.reviews__slider-nav-arrow--next');
+
+prevButton.addEventListener('click', () => splide.go('<'));
+nextButton.addEventListener('click', () => splide.go('>'));
+
+updateArrowState();
+
+splide.on('move', updateArrowState);
+splide.on('updated', updateArrowState); 
+
+function updateArrowState() {
+  if (splide.index === 0) {
+    prevButton.classList.add('reviews__slider-nav-arrow--is-disabled');
+  } else {
+    prevButton.classList.remove('reviews__slider-nav-arrow--is-disabled');
+  }
+
+  const lastSlideIndex = splide.Components.Controller.getEnd();
+
+  if (splide.index === lastSlideIndex) {
+    nextButton.classList.add('reviews__slider-nav-arrow--is-disabled');
+  } else {
+    nextButton.classList.remove('reviews__slider-nav-arrow--is-disabled');
+  }
+}
+
+
+// range input
+const rangeInput = document.querySelector('.prise__form-range');
+const sumDisplay = document.querySelector('.prise__form-sum'); 
+const max = parseInt(rangeInput.max, 10);
+const step = parseInt(rangeInput.step, 10);
+
+rangeInput.value = 0;
+updateRangeValue();
+
+function updateRangeValue() {
+  const value = rangeInput.value;
+  sumDisplay.textContent = `${value}`; 
+
+  const percent = (value / max) * 100;
+
+  rangeInput.style.background = `linear-gradient(to right, #2b71e3 ${percent}%, #ddd ${percent}%)`;
+
+  const sliderWidth = rangeInput.offsetWidth;
+  const positionInPx = (percent / 100) * sliderWidth;
+
+  sumDisplay.style.left = `${positionInPx}px`;
+}
+
+rangeInput.addEventListener('input', updateRangeValue);
